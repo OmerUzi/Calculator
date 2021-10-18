50b7b9e64a92df62e75e86feddfcd0c1b261ef7c
omer uzi <omer135798@gmail.com>
2021-10-18 10:06:11 +0300

    create basic fjs function

---
+++ script.js
@@ -0,0 +1,30 @@
+const add = function(a,b) {
+    return a+b;
+      
+  };
+  
+  const subtract = function(a,b) {
+    return a-b;
+      
+  };
+  
+  
+  const multiply = function(a,b) {
+    return a*b;  
+  };
+  
+  
+  const divide = function(a,b) {
+    return a/b;
+      
+  };
+  
+  function operate(a,b,operator){
+      if(operator == "+") return add(a,b);
+      else if(operator == "-") return subtract(a,b);
+      else if(operator == "/") return divide(a,b);
+      else if(operator=="*") return multiply(a,b);
+
+      return "error";
+   }
+   
